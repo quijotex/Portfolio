89404452b1af19dfb07454e94f7b5d36ae4118da
Mario Martinez <mariopresiga@gmail.com>
2023-08-10 20:08:50 -0500

    Work in progress

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,7 +1,12 @@
+import photo from '../assets/Images/ProfilePhoto.webp'
+
 const AboutMe = () => {
     return(
         <main className="aboutme">
         <div className="background-name"/>
+        <div className='photo-me'>
+            <img src={photo} alt="" />
+        </div>
         <div className="background-links"/>
 
         </main>
