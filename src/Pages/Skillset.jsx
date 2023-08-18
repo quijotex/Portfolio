c4a14f9d4bfed5a8c11fa7719f9b61e7ce3a6a76
Mario Martinez <mariopresiga@gmail.com>
2023-08-18 01:12:04 -0500

    Skillset is already done

--- src/Pages/Skillset.jsx
+++ src/Pages/Skillset.jsx
@@ -1,8 +1,42 @@
+import skillset from "../assets/Images/Skillset.svg"
 const Skillset = () => {
     return(
-        <>
-        <h2>Skillset</h2>
-        </>
+        <section className="skillset">
+            <div className="skillset-skills">
+                <div>
+                    <div className="skill-img">
+                    <img src={skillset} alt=""/>
+                    </div>
+                    <h3>Languages</h3>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>CSS</li>
+                        <li>HTML5</li>
+                    </ul>
+                </div>
+                <div className="skillset-skills--below">
+                    <div>
+                        <div className="skill-img">
+                            <img src={skillset} alt=""/>
+                        </div>
+                        <h3>Frameworks</h3>
+                        <ul>
+                            <li>React</li>
+                            <li>Boostrap</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <div className="skill-img">
+                            <img src={skillset} alt=""/>
+                        </div>
+                        <h3>Source Control</h3>
+                        <ul>
+                            <li>Git/GitHub</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Skillset
